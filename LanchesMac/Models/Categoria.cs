e0f42c2d9bcb7ab32c2b2761485315ee8c61fb90
Hitalo Jacome <jacomehitalo@gmail.com>
2024-01-16 14:25:00 -0300

    Adding EF Core

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,18 +1,16 @@
-﻿namespace LanchesMac.Models
+﻿namespace LanchesMac.Models;
+public class Categoria
 {
-    public class Categoria
-    {
-        public int CategoriaId { get; set; }
-        public string? CategoriaNome { get; set; }
-        public string? Descricao { get; set; }
-        public List<Lanche>? Lanches { get; set; }
+    public int CategoriaId { get; set; }
+    public string? CategoriaNome { get; set; }
+    public string? Descricao { get; set; }
+    public List<Lanche>? Lanches { get; set; }
 
-        public Categoria(int categoriaId, string? categoriaNome, string? descricao, List<Lanche>? lanches)
-        {
-            CategoriaId = categoriaId;
-            CategoriaNome = categoriaNome;
-            Descricao = descricao;
-            Lanches = lanches;
-        }
+    public Categoria(int categoriaId, string? categoriaNome, string? descricao, List<Lanche>? lanches)
+    {
+        CategoriaId = categoriaId;
+        CategoriaNome = categoriaNome;
+        Descricao = descricao;
+        Lanches = lanches;
     }
 }
