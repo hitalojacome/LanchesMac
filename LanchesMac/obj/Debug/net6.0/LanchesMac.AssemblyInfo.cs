f74b9aff8956ac605a7b1717abe108330d02a4f9
Hitalo Jacome <jacomehitalo@gmail.com>
2024-02-27 23:21:08 -0300

    criando controller de Lanches

--- LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab97d90d26beadc4ef2dd9690a08a091e2c504f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
