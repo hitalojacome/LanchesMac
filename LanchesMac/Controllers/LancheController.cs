b49cca520468b97298735d39648aa8f364ea5299
HITALO JACOME <jacomehitalo@gmail.com>
2024-02-28 14:10:39 -0300

    criação de controller e view para lanches

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -5,16 +5,22 @@ namespace LanchesMac.Controllers
 {
     public class LancheController : Controller
     {
+        // Declaração de uma variável privada para armazenar a instância do repositório de lanches
         private readonly ILancheRepository _lancheRepository;
 
+        // Construtor da classe LancheController que recebe uma instância de ILancheRepository
         public LancheController(ILancheRepository lancheRepository)
         {
+            // Atribui o argumento recebido ao campo privado
             _lancheRepository = lancheRepository;
         }
 
+        // Método de ação para listar os lanches
         public IActionResult List()
         {
+            // Chama o método Lanches do repositório para obter a lista de lanches
             var lanches = _lancheRepository.Lanches;
+            // Retorna uma View com a lista de lanches como modelo
             return View(lanches);
         }
     }
