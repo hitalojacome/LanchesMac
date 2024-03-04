efe908e5554150ae568ba8c087943041665457e1
Hitalo Jacome <jacomehitalo@gmail.com>
2024-03-04 15:04:49 -0300

    Método GetCarrinho

--- LanchesMac/Models/CarrinhoCompra.cs
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -17,5 +17,27 @@ namespace LanchesMac.Models
         public string CarrinhoCompraId { get; set; }
         // Lista de itens
         public List<CarrinhoCompraItem>? CarrinhoCompraItens { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            // Define uma sessão
+            ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
+
+            // Obtem um serviço do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+
+            // Obtem ou gera o Id do carrinho
+            var carrinhoId = session?.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            // Atribui o id do carrinho na sessão
+            session?.SetString("CarrinhoId", carrinhoId);
+
+            // Retorna o carrinho com o contexto e o Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+
+        }
     }
 }
