d6f80f253660bd1c8777ea05c031b5bd6643481d
Hitalo Jacome <jacomehitalo@gmail.com>
2024-03-11 17:16:28 -0300

    otimizando código do controller para exibição de categoria

--- LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af499e3fdcbf8ee1a5ce4731195668c4fa3493e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07f65eea98b9409eff0287b32caa9c45e756eba")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
