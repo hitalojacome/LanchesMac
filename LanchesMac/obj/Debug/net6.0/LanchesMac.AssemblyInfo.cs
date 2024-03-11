a0efd817237ad83a2f9c5e6c6afe17382270816a
Hitalo Jacome <jacomehitalo@gmail.com>
2024-03-11 09:42:17 -0300

    Exibição de lanches preferidos

--- LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
+++ LanchesMac/obj/Debug/net6.0/LanchesMac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f50c3ae0b00192675c20fbd676cc6183b26e2ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanchesMac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
